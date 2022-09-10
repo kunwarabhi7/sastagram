fbd6e360f9d4bc50b42a7b8b10b5ba1812b3c9e1
Abhishek <abhishekparmar162@gail.com>
2022-09-10 09:35:27 +0530

    fix a bug

--- src/fireebase.config.js
+++ src/fireebase.config.js
@@ -4,13 +4,15 @@ import { getFirestore } from 'firebase/firestore'
 import { GoogleAuthProvider, getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhN0dAihzaesPZTnDv0HpPQSq0624zubI",
-  authDomain: "sastagram-7bd67.firebaseapp.com",
-  projectId: "sastagram-7bd67",
-  storageBucket: "sastagram-7bd67.appspot.com",
-  messagingSenderId: "892672151427",
-  appId: "1:892672151427:web:de4cbc4311c4dc8acf70b6"
-};
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+
+}
+
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app)
